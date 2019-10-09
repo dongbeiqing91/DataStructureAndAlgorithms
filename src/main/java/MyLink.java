0514306680742a077dbfe04339f3446e96bc6f41
Dong Beiqing <bedong@ebay.com>
2019-10-09 16:54:03 +0800

    first commit

---
+++ src/main/java/MyLink.java
@@ -0,0 +1,147 @@
+/**
+ * @author Bei
+ * @date 2019-10-09
+ */
+
+public class MyLink {
+    Node head = null;
+
+    public void addNode(int d) {
+        Node newNode = new Node(d);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        tmp.next = newNode;
+    }
+
+    public boolean deleteNode(int index) {
+        if (index < 1 || index > length()) {
+            return false;
+        }
+        if (index == 1) {
+            head = head.next;
+            return true;
+        }
+        int i = 1;
+        Node preNode = head;
+        Node curNode = preNode.next;
+        while (curNode != null) {
+            if (i == index) {
+                preNode.next = curNode.next;
+                return true;
+            }
+            preNode = curNode;
+            curNode = curNode.next;
+            i++;
+        }
+        return false;
+    }
+
+    public int length() {
+        int length = 0;
+        Node tmp = head;
+        while (tmp != null) {
+            length++;
+            tmp = tmp.next;
+        }
+        return length;
+    }
+
+    public boolean deleteNode11(Node n) {
+        if (n == null || n.next == null) {
+            return false;
+        }
+        int tmp = n.data;
+        n.data = n.next.data;
+        n.next.data = tmp;
+        n.next = n.next.next;
+        System.out.println("删除成功！");
+        return true;
+    }
+
+    public void printList() {
+        Node tmp = head;
+        while (tmp != null) {
+            System.out.println(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+
+    public void printListReversely(Node pListHead) {
+        if (pListHead != null) {
+            printListReversely(pListHead.next);
+            System.out.println("printListReversely:" + pListHead.data);
+        }
+    }
+
+    /**
+     * 判断链表是否有环，单向链表有环时，尾节点相同
+     *
+     * @param head
+     * @return
+     */
+    public boolean IsLoop(Node head) {
+        Node fast = head, slow = head;
+        if (fast == null) {
+            return false;
+        }
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                System.out.println("该链表有环");
+                return true;
+            }
+        }
+        return !(fast == null || fast.next == null);
+    }
+
+    /**
+     * 找出链表环的入口
+     *
+     * @param head
+     * @return
+     */
+    public Node FindLoopPort(Node head) {
+        Node fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+
+    public static void main(String[] args) {
+        MyLink list = new MyLink();
+        list.addNode(5);
+        list.addNode(3);
+        list.addNode(1);
+        list.addNode(2);
+        list.addNode(55);
+        list.addNode(36);
+        System.out.println("linkLength:" + list.length());
+        System.out.println("head.data:" + list.head.data);
+        list.printList();
+        list.deleteNode(4);
+        System.out.println("After deleteNode(4):");
+        list.printList();
+    }
+
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,12 @@
+/**
+ * @author Bei
+ * @date 2019-10-09
+ */
+public class Node {
+    Node next = null;
+    public int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
