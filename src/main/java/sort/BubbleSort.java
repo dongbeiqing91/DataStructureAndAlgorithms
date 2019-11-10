3c246bf177b2673c7dfaad1d9ea0bee637561877
unknown <350758787@qq.com>
2019-11-10 14:36:45 +0800

    sort

---
+++ src/main/java/sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    private int[] sortAsc(int[] toBeSorted) {
+        for (int i = 0; i < toBeSorted.length; i++) {
+            for (int j = 0; j < toBeSorted.length - i - 1; j++) {
+                if (toBeSorted[j] >= toBeSorted[j + 1]) {
+                    int temp = toBeSorted[j];
+                    toBeSorted[j] = toBeSorted[j + 1];
+                    toBeSorted[j + 1] = temp;
+                }
+            }
+        }
+        return toBeSorted;
+    }
+
+    public static void main(String[] args) {
+        int[] toBeSorted = {5, 4, 3, 2, 1};
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] ints = bubbleSort.sortAsc(toBeSorted);
+        System.out.println(Arrays.toString(ints));
+    }
+}
